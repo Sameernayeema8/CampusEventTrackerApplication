8fed3045e4869ba7e232f73e774c4dac5eeea8ef
Sameernayeema8 <nadeemsameernayeem@gmail.com>
2025-08-14 17:55:44 +0530

    Create missing package.json and project files

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Campus Event Tracker</h1>
+    <p>Welcome to the Campus Event Tracker Application</p>
+  </div>
+`
